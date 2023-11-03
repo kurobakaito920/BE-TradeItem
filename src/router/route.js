76a2c875547a4cbaec966d74a51af8cc07fe6020
Sliance <sliance2402@gmail.com>
2023-11-03 15:21:31 +0700

    update route subcate

--- src/router/route.js
+++ src/router/route.js
@@ -1,10 +1,13 @@
 import routeUser from "./user.js";
 import routeCate from "./cate.js";
-
+import routeSubCate from "./subcate.js";
+import routeProd from "./prod.js";
 
 const app = (app) =>{
     app.use("/api/auth", routeUser);
     app.use("/api/cate", routeCate);
+    app.use("/api/subcate", routeSubCate);
+    app.use("/api/prod", routeProd);
 }
 
 module.exports = app;
