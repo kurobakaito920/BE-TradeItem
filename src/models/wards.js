3653ec1029a78c5c25abce9abc09dbb020f9fb51
Sliance <sliance2402@gmail.com>
2023-11-06 19:47:54 +0700

    fixing belongTo disctrictID

--- src/models/wards.js
+++ src/models/wards.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.belongsTo(models.Disctricts,{
+      this.belongsTo(models.Districts,{
         foreignKey: 'districtID',
         targetKey: 'id'
       });
