5be52b47e51ba4ff0dde2bc554c2e2a99f050ae4
Sliance <sliance2402@gmail.com>
2023-11-08 00:12:14 +0700

    add new route city

--- src/router/route.js
+++ src/router/route.js
@@ -2,12 +2,14 @@ import routeUser from "./user.js";
 import routeCate from "./cate.js";
 import routeSubCate from "./subcate.js";
 import routeProd from "./prod.js";
+import routeCity from "./city.js";
 
 const app = (app) =>{
     app.use("/api/auth", routeUser);
     app.use("/api/cate", routeCate);
     app.use("/api/subcate", routeSubCate);
     app.use("/api/prod", routeProd);
+    app.use("/api/city", routeCity);
 }
 
 module.exports = app;
