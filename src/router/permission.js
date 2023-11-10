d837df8cc6cc957af4d97cf0a743fcb54f4cdd48
kiet <kietphanco@gmail.com>
2023-11-10 17:30:18 +0700

    route permission done

---
+++ src/router/permission.js
@@ -0,0 +1,10 @@
+import express from "express";
+import perController from "../controller/permission_controller.js";
+const route = express.Router();
+
+route.get('/perAPI', perController.allPermission);
+route.post('/createPer', perController.storePermission);
+route.put('/updatePer/:id', perController.updatePermission);
+route.delete('/deletePer/:id', disController.deletePermission);
+
+module.exports = route;
