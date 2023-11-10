731924fb1c924ab60216b56089d74e70ba571a5d
kiet <kietphanco@gmail.com>
2023-11-10 17:26:27 +0700

    update route district & add route ward

--- src/router/route.js
+++ src/router/route.js
@@ -4,6 +4,8 @@ import routeSubCate from "./subcate.js";
 import routeProd from "./prod.js";
 import routeCity from "./city.js";
 import routeDis from "./district.js";
+import routeWard from "./ward.js";
+import routePer from "./permission.js";
 
 const app = (app) =>{
     app.use("/api/auth", routeUser);
@@ -12,6 +14,8 @@ const app = (app) =>{
     app.use("/api/prod", routeProd);
     app.use("/api/city", routeCity);
     app.use("/api/district", routeDis);
+    app.use("/api/ward", routeWard);
+    app.use("/api/permission", routePer);
 }
 
 module.exports = app;
