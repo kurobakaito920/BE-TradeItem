e46de730a1aaca27fd2ea635f18ce5b8720a6948
Sliance <sliance2402@gmail.com>
2023-11-16 10:33:07 +0700

    add new table rates

---
+++ src/models/rates.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Rates extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      this.belongsTo(models.News,{
+        foreignKey: 'newsID',
+        targetKey: 'id',
+      });
+      this.belongsTo(models.Users,{
+        foreignKey: 'usersID',
+        targetKey: 'id',
+      });
+    }
+  }
+  Rates.init({
+    valueRate: DataTypes.INTEGER,
+    timeRate: DataTypes.STRING,
+    newsID: DataTypes.INTEGER,
+    userID: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Rates',
+  });
+  return Rates;
+};
