b9f304d9898c5e40ee65a3c083904dc74d40868c
Sliance <sliance2402@gmail.com>
2023-11-16 10:29:46 +0700

    change name route permission

--- src/router/permission.js
+++ src/router/permission.js
@@ -5,6 +5,6 @@ const route = express.Router();
 route.get('/perAPI', perController.allPermission);
 route.post('/createPer', perController.storePermission);
 route.put('/updatePer/:id', perController.updatePermission);
-route.delete('/deletePer/:id', disController.deletePermission);
+route.delete('/deletePer/:id', perController.deletePermission);
 
 module.exports = route;
